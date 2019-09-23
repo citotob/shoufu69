953fa11543c6a7fcb342ad99eb9dcc34395ea1ba
christopher <erdell_tog@yahoo.com>
2019-09-23 20:24:53 +0800

    1

--- editors/admin.py
+++
@@ -1,7 +0,0 @@
-# encoding: utf-8
-from django.contrib import admin
-from editors.models import RichTextModel
-
-
-admin.site.register(RichTextModel)
-
--- menu/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import ugettext_lazy as _
-
-
-class MenuConfig(AppConfig):
-    name = 'menu'
-    verbose_name = _('Menu')
